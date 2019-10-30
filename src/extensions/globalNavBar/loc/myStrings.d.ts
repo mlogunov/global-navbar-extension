726f19afda15b492789d1bc1bff69bb7ec6723b5
Maxim Logunov <m.logunov@outlook.com>
2019-10-30 10:02:42 +0300

    Empty project

---
+++ src/extensions/globalNavBar/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IGlobalNavBarApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'GlobalNavBarApplicationCustomizerStrings' {
+  const strings: IGlobalNavBarApplicationCustomizerStrings;
+  export = strings;
+}
